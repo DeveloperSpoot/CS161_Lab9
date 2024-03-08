603d301ad15714c1f9cabb7426ea1431634e0b14
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-03-08 15:37:33 -0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace CS161_Lab9
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox4 = new GroupBox();
+            section1AverageLabel = new Label();
+            section1ListBox = new ListBox();
+            groupBox2 = new GroupBox();
+            groupBox5 = new GroupBox();
+            section2AverageLabel = new Label();
+            section2ListBox = new ListBox();
+            groupBox3 = new GroupBox();
+            groupBox6 = new GroupBox();
+            section3AverageLabel = new Label();
+            section3ListBox = new ListBox();
+            groupBox7 = new GroupBox();
+            averageLabel = new Label();
+            groupBox8 = new GroupBox();
+            groupBox10 = new GroupBox();
+            highestSectionLabel = new Label();
+            groupBox9 = new GroupBox();
+            highestScoreLabel = new Label();
+            groupBox11 = new GroupBox();
+            groupBox12 = new GroupBox();
+            lowestSectionLabel = new Label();
+            groupBox13 = new GroupBox();
+            lowestScoreLabel = new Label();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox5.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox6.SuspendLayout();
+            groupBox7.SuspendLayout();
+            groupBox8.SuspendLayout();
+            groupBox10.SuspendLayout();
+            groupBox9.SuspendLayout();
+            groupBox11.SuspendLayout();
+            groupBox12.SuspendLayout();
+            groupBox13.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(groupBox4);
+            groupBox1.Controls.Add(section1ListBox);
+            groupBox1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox1.Location = new Point(12, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(180, 323);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Section 1";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(section1AverageLabel);
+            groupBox4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox4.Location = new Point(11, 261);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(156, 50);
+            groupBox4.TabIndex = 1;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Average";
+            // 
+            // section1AverageLabel
+            // 
+            section1AverageLabel.BackColor = SystemColors.ControlLight;
+            section1AverageLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            section1AverageLabel.Location = new Point(6, 19);
+            section1AverageLabel.Name = "section1AverageLabel";
+            section1AverageLabel.Size = new Size(144, 23);
+            section1AverageLabel.TabIndex = 0;
+            section1AverageLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // section1ListBox
+            // 
+            section1ListBox.Font = new Font("Microsoft Sans Serif", 9F);
+            section1ListBox.FormattingEnabled = true;
+            section1ListBox.ItemHeight = 15;
+            section1ListBox.Location = new Point(11, 23);
+            section1ListBox.Name = "section1ListBox";
+            section1ListBox.Size = new Size(156, 229);
+            section1ListBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(groupBox5);
+            groupBox2.Controls.Add(section2ListBox);
+            groupBox2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox2.Location = new Point(198, 22);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(180, 323);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Section 2";
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(section2AverageLabel);
+            groupBox5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox5.Location = new Point(11, 261);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(156, 50);
+            groupBox5.TabIndex = 2;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Average";
+            // 
+            // section2AverageLabel
+            // 
+            section2AverageLabel.BackColor = SystemColors.ControlLight;
+            section2AverageLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            section2AverageLabel.Location = new Point(6, 19);
+            section2AverageLabel.Name = "section2AverageLabel";
+            section2AverageLabel.Size = new Size(144, 23);
+            section2AverageLabel.TabIndex = 0;
+            section2AverageLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // section2ListBox
+            // 
+            section2ListBox.Font = new Font("Microsoft Sans Serif", 9F);
+            section2ListBox.FormattingEnabled = true;
+            section2ListBox.ItemHeight = 15;
+            section2ListBox.Location = new Point(11, 23);
+            section2ListBox.Name = "section2ListBox";
+            section2ListBox.Size = new Size(156, 229);
+            section2ListBox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(groupBox6);
+            groupBox3.Controls.Add(section3ListBox);
+            groupBox3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox3.Location = new Point(384, 22);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(180, 323);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Section 3";
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(section3AverageLabel);
+            groupBox6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox6.Location = new Point(11, 261);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(156, 50);
+            groupBox6.TabIndex = 2;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Average";
+            // 
+            // section3AverageLabel
+            // 
+            section3AverageLabel.BackColor = SystemColors.ControlLight;
+            section3AverageLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            section3AverageLabel.Location = new Point(6, 19);
+            section3AverageLabel.Name = "section3AverageLabel";
+            section3AverageLabel.Size = new Size(144, 23);
+            section3AverageLabel.TabIndex = 0;
+            section3AverageLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // section3ListBox
+            // 
+            section3ListBox.Font = new Font("Microsoft Sans Serif", 9F);
+            section3ListBox.FormattingEnabled = true;
+            section3ListBox.ItemHeight = 15;
+            section3ListBox.Location = new Point(11, 23);
+            section3ListBox.Name = "section3ListBox";
+            section3ListBox.Size = new Size(156, 229);
+            section3ListBox.TabIndex = 0;
+            // 
+            // groupBox7
+            // 
+            groupBox7.Controls.Add(averageLabel);
+            groupBox7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox7.Location = new Point(12, 351);
+            groupBox7.Name = "groupBox7";
+            groupBox7.Size = new Size(552, 59);
+            groupBox7.TabIndex = 3;
+            groupBox7.TabStop = false;
+            groupBox7.Text = "Average of All Sections";
+            // 
+            // averageLabel
+            // 
+            averageLabel.BackColor = SystemColors.ControlLight;
+            averageLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            averageLabel.Location = new Point(11, 19);
+            averageLabel.Name = "averageLabel";
+            averageLabel.Size = new Size(528, 29);
+            averageLabel.TabIndex = 1;
+            averageLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox8
+            // 
+            groupBox8.Controls.Add(groupBox10);
+            groupBox8.Controls.Add(groupBox9);
+            groupBox8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox8.Location = new Point(12, 416);
+            groupBox8.Name = "groupBox8";
+            groupBox8.Size = new Size(254, 139);
+            groupBox8.TabIndex = 4;
+            groupBox8.TabStop = false;
+            groupBox8.Text = "Highest Score";
+            // 
+            // groupBox10
+            // 
+            groupBox10.Controls.Add(highestSectionLabel);
+            groupBox10.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox10.Location = new Point(11, 78);
+            groupBox10.Name = "groupBox10";
+            groupBox10.Size = new Size(234, 51);
+            groupBox10.TabIndex = 2;
+            groupBox10.TabStop = false;
+            groupBox10.Text = "Section";
+            // 
+            // highestSectionLabel
+            // 
+            highestSectionLabel.BackColor = SystemColors.ControlLight;
+            highestSectionLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            highestSectionLabel.Location = new Point(6, 19);
+            highestSectionLabel.Name = "highestSectionLabel";
+            highestSectionLabel.Size = new Size(222, 23);
+            highestSectionLabel.TabIndex = 1;
+            highestSectionLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox9
+            // 
+            groupBox9.Controls.Add(highestScoreLabel);
+            groupBox9.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox9.Location = new Point(11, 21);
+            groupBox9.Name = "groupBox9";
+            groupBox9.Size = new Size(234, 51);
+            groupBox9.TabIndex = 0;
+            groupBox9.TabStop = false;
+            groupBox9.Text = "Score";
+            // 
+            // highestScoreLabel
+            // 
+            highestScoreLabel.BackColor = SystemColors.ControlLight;
+            highestScoreLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            highestScoreLabel.Location = new Point(6, 19);
+            highestScoreLabel.Name = "highestScoreLabel";
+            highestScoreLabel.Size = new Size(222, 23);
+            highestScoreLabel.TabIndex = 1;
+            highestScoreLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox11
+            // 
+            groupBox11.Controls.Add(groupBox12);
+            groupBox11.Controls.Add(groupBox13);
+            groupBox11.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox11.Location = new Point(310, 416);
+            groupBox11.Name = "groupBox11";
+            groupBox11.Size = new Size(254, 139);
+            groupBox11.TabIndex = 5;
+            groupBox11.TabStop = false;
+            groupBox11.Text = "Lowest Score";
+            // 
+            // groupBox12
+            // 
+            groupBox12.Controls.Add(lowestSectionLabel);
+            groupBox12.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox12.Location = new Point(11, 78);
+            groupBox12.Name = "groupBox12";
+            groupBox12.Size = new Size(234, 51);
+            groupBox12.TabIndex = 2;
+            groupBox12.TabStop = false;
+            groupBox12.Text = "Section";
+            // 
+            // lowestSectionLabel
+            // 
+            lowestSectionLabel.BackColor = SystemColors.ControlLight;
+            lowestSectionLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            lowestSectionLabel.Location = new Point(6, 19);
+            lowestSectionLabel.Name = "lowestSectionLabel";
+            lowestSectionLabel.Size = new Size(222, 23);
+            lowestSectionLabel.TabIndex = 1;
+            lowestSectionLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox13
+            // 
+            groupBox13.Controls.Add(lowestScoreLabel);
+            groupBox13.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            groupBox13.Location = new Point(11, 21);
+            groupBox13.Name = "groupBox13";
+            groupBox13.Size = new Size(234, 51);
+            groupBox13.TabIndex = 0;
+            groupBox13.TabStop = false;
+            groupBox13.Text = "Score";
+            // 
+            // lowestScoreLabel
+            // 
+            lowestScoreLabel.BackColor = SystemColors.ControlLight;
+            lowestScoreLabel.Font = new Font("Microsoft Sans Serif", 9F);
+            lowestScoreLabel.Location = new Point(6, 19);
+            lowestScoreLabel.Name = "lowestScoreLabel";
+            lowestScoreLabel.Size = new Size(222, 23);
+            lowestScoreLabel.TabIndex = 1;
+            lowestScoreLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Tomato;
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(12, 561);
+            button1.Name = "button1";
+            button1.Size = new Size(552, 36);
+            button1.TabIndex = 6;
+            button1.Text = "EXIT";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(576, 608);
+            Controls.Add(button1);
+            Controls.Add(groupBox11);
+            Controls.Add(groupBox8);
+            Controls.Add(groupBox7);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Main";
+            Text = "Main";
+            Load += Main_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox5.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox6.ResumeLayout(false);
+            groupBox7.ResumeLayout(false);
+            groupBox8.ResumeLayout(false);
+            groupBox10.ResumeLayout(false);
+            groupBox9.ResumeLayout(false);
+            groupBox11.ResumeLayout(false);
+            groupBox12.ResumeLayout(false);
+            groupBox13.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox4;
+        private Label section1AverageLabel;
+        private ListBox section1ListBox;
+        private GroupBox groupBox2;
+        private GroupBox groupBox5;
+        private Label section2AverageLabel;
+        private ListBox section2ListBox;
+        private GroupBox groupBox3;
+        private GroupBox groupBox6;
+        private Label section3AverageLabel;
+        private ListBox section3ListBox;
+        private GroupBox groupBox7;
+        private Label averageLabel;
+        private GroupBox groupBox8;
+        private GroupBox groupBox10;
+        private Label highestSectionLabel;
+        private GroupBox groupBox9;
+        private Label highestScoreLabel;
+        private GroupBox groupBox11;
+        private GroupBox groupBox12;
+        private Label lowestSectionLabel;
+        private GroupBox groupBox13;
+        private Label lowestScoreLabel;
+        private Button button1;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,123 @@
+using System.Security.AccessControl;
+
+namespace CS161_Lab9
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private double[][] SECTIONS = new double[3][];         //Initalizing our jagged array.
+
+        private void Main_Load(object sender, EventArgs e) //Our onLoad event method.
+        {
+            // Initializing our sub arrays in our jagged array.
+            SECTIONS[0] = new double[12];
+            SECTIONS[1] = new double[8];
+            SECTIONS[2] = new double[10];
+
+
+            //Vairables to track the lowest and highest scores.
+            double highestScore = 0.00;
+            int highestSection = 0;
+
+            double lowestScore = 100000.00;
+            int lowestSection = 0;
+
+            try
+            {
+                // Creating an array of our section files and section ListBoxs.
+                StreamReader[] sectionFiles = { File.OpenText("Section1.txt"), File.OpenText("Section2.txt"), File.OpenText("Section3.txt") };
+                ListBox[] sectionListBoxs = { section1ListBox, section2ListBox, section3ListBox };
+
+
+                int fileIndex = 0; // Tracking which file we are currently reading.
+                foreach (var file in sectionFiles) // Looping though all three files.
+                {
+                    int index = 0; // Tracking each line we are.
+                    while (!file.EndOfStream) // Looping through each file.
+                    {
+                        try
+                        {
+                            string currentLine = file.ReadLine(); // Getting the current score on the current line.
+
+                            double currentScore = double.Parse(currentLine); // Parsing said score into a double.
+
+
+                            SECTIONS[fileIndex][index] = currentScore; // Updating our jagged array with the score.
+
+                            sectionListBoxs[fileIndex].Items.Add(currentLine); // Displaying the score in the appropriate section list box.
+
+
+                            if (highestScore < currentScore) // Checking for the highest score.
+                            {
+                                // Upadting the highest score.
+                                highestScore = currentScore;
+                                highestSection = fileIndex;
+                            }
+
+                            if(lowestScore > currentScore) // Checking for the lowest score.
+                            {
+                                //Updating the lowest score.
+                                lowestScore = currentScore;
+                                lowestSection = fileIndex;
+                            }
+
+                            index++; // Updating the line index.
+                        }catch(Exception ex)
+                        {
+                            MessageBox.Show(ex.Message); // Displayin
+                        }
+                    }
+
+                    sectionFiles[fileIndex].Close(); // Closing each file once we are done reading it,
+
+                    calculateSectionAverage(fileIndex); // Calculating the section average.
+                    fileIndex++; // Updating our file index.
+                }
+
+                calculateAverage(); // Calculating the overall average.
+
+                highestScoreLabel.Text = highestScore.ToString("n2"); //Displaying highest score.
+                highestSectionLabel.Text = (highestSection+1).ToString();
+
+                lowestScoreLabel.Text = lowestScore.ToString("n2"); // Displaying lowest score,
+                lowestSectionLabel.Text = (lowestSection+1).ToString();
+
+
+            }
+            catch(Exception ex) // Catching any incompetence.
+            {
+                MessageBox.Show(ex.Message); // Displaying any errors.
+            }
+        }
+
+        private void calculateSectionAverage(int section) // Method to calculate the section average, that has an argument for the section to calculate.
+        {
+            Label[] sectionAverageLabels = { section1AverageLabel, section2AverageLabel, section3AverageLabel }; // List of average labels for easy access.
+            
+            double totalScore = 0.00;
+            foreach (var score in SECTIONS[section]) // Looping through each score
+            {
+                totalScore += score; // Adding each score to the total for the section.
+            }
+
+            double averageScore = totalScore / SECTIONS[section].Length; // Calculating the score.
+
+            sectionAverageLabels[section].Text = averageScore.ToString("n2"); // Displaying the score.
+        }
+
+        private void calculateAverage() // Method to calculate the overall vaerage.
+        {
+            double averageScoreSec1 = double.Parse(section1AverageLabel.Text); // Getting each score average from each section.
+            double averageScoreSec2 = double.Parse(section2AverageLabel.Text);
+            double averageScoreSec3 = double.Parse(section3AverageLabel.Text);
+
+            double average = (averageScoreSec1 + averageScoreSec2 + averageScoreSec3) / 3; // Calculating the overall average.
+
+            averageLabel.Text = average.ToString("n2"); // Diplaying the average.
+        }
+    }
+}
